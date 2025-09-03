e4181a7bdce17495b8ff3cea6df163f2957a5181
Ole Kodehode <ole.kodehode@gmail.com>
2025-09-03 10:36:20 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,20 @@
+/* 
+
+random time traveler card:
+
+image from a selection of +20
+random date of birth. Between 2100 and 3100 year.
+Where they are currently - Past or Future. 1000 BC  / 2090 AD 
+or at least ~70 years in the future from their date of birth until 5000 AD.
+Known for (could be profession). +50 things? 
+Short time travel quote? +20 quotes? 
+
+No presistency - New selection on every refresh
+*/
+
+// Array of available portraits
+const portraits = [];
+// Array of quotes for time travelers to the past
+const pastQuotes = [];
+// Array of quotes for time travelers to the future
+const futureQuotes = [];
